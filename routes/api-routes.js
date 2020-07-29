4e2c2b583ef91cb301f01d036be4d9011f99a570
yulia tikhonova <yuliatikhonova@outlook.com>
2020-07-29 16:28:36 -0700

    finished adding all the api routes and had to convert all the apps to routers so it does not crash. Added the seeds

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,37 +1,44 @@
+const router = require('express').Router();
+
 const db = require("../models");
 
 
-module.exports = function (app) {
-    app.get("/workouts", (req, res) => {
-        db.Workout.find({})
-            .then(dbWorkout => {
-                res.json(dbWorkout);
-            })
-            .catch(err => {
-                res.json(err);
-            });
+router.get("/workouts", (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
     });
+});
 
 
-    app.put('/workouts/:id', (req, res) => {
-        db.Workout.findByIdAndUpdate(req.params.id, {
-          $push: {
-            exercises: req.body
-          }
-        }).then(dbWorkout => {
-          res.json(dbWorkout);
-      })
-        .catch((err) => {
-          res.json(err);
-        });
-      });
+router.put('/workouts/:id', (req, res) => {
+  db.Workout.findByIdAndUpdate(req.params.id, {
+    $push: {
+      exercises: req.body
+    }
+  }).then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 
+router.get("/workouts/range", (req, res) => {
+  db.Workout.find({}).limit(7)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-
-    
-
-};
+module.exports = router;
 
 
 
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,21 +1,21 @@
 const path = require('path');
+const router = require('express').Router();
 
-module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-    
-    app.get('/exercise', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/exercise.html'));
-    });
 
-    app.get('/exercise?', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/exercise.html'));
-    });
-    
-    app.get('/stats', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/stats.html'));
-    });
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+router.get('/exercise?', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
   
-  };
-  
+module.exports = router;
